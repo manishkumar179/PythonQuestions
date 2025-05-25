49c7ad37400972af23090c34042cb9d843f3c1d3
Manish Kumar <192117778+manishkumar179@users.noreply.github.com>
2025-05-25 12:38:34 +0530

    Print the basic statement using python language

---
+++ Program1.py
@@ -0,0 +1,7 @@
+gender=str(input("Enter your gender: "))
+if(gender=='male' or gender=='Male'):
+    print("Good Morning Sir")
+elif(gender=='female' or gender=='Female'):
+    print("Good Morning ma'am")
+else:
+    print("You Entered Wrong input")
