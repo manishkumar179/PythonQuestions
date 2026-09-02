a5282451522ddcf80146df161c3c9718166663ac
Manish Kumar <mk0437271@gmail.com>
2026-09-03 00:19:07 +0530

    Data Types in python

---
+++ DataTypes.py
@@ -0,0 +1,48 @@
+a = 10
+print(type(a))
+
+b = 10.34
+print(type(b))
+
+c = float(a)
+print(c)
+
+# Complex number
+
+com = 3 + 4j
+
+print(com)
+print(type(com))
+
+# String
+str = 'mani'
+print(str)
+print(type(str))
+
+# Boolean
+
+x = 1
+y = 4
+greater = x > y
+print(greater)
+
+greater = x < y
+print(greater)
+
+# Convert boolean into integer
+
+xy = int (True)
+print("True value in integer "  ,xy)
+
+xy = int(False)
+print("False value in integer ",xy)
+
+
+# range(start, endExclude, stepToMove);
+
+r = range (2, 10, 2)
+print(set(r))
+
+
+
+
